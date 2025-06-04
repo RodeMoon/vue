1bf1e04f460d4a9deaff3d3da40097b4ac87cd16
kean <323030001@upjr.edu.mx>
2025-06-04 07:59:50 -0600

    fix: vue-coffee

--- vue-coffee/src/App.vue
+++ vue-coffee/src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <h1>Welcome to Vue Coffee</h1>
-    <section>
-      <h3>Count: {{ count }}</h3>
-      <h3>Square: {{ square }}</h3>
-      <button @click="count++">+ 1</button>
-      <button @click="count--">- 1</button>
-    </section>
+    <myclass />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-const count = ref(0);
-const square = computed(() => count.value * count.value);
+import myclass from './state_variable.vue';
 </script>
 
 <style scoped></style>
---
+++ vue-coffee/src/state_variable.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to Vue Coffee</h1>
+    <section>
+      <h3>Count: {{ count }}</h3>
+      <h3>Square: {{ square }}</h3>
+      <button @click="count++">+ 1</button>
+      <button @click="count--">- 1</button>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+const count = ref(0);
+const square = computed(() => count.value * count.value);
+</script>
+
+<style scoped></style>
