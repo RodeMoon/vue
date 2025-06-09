4162400f2f907af97af5c48c543b10f5c442a8f8
kean <323030001@upjr.edu.mx>
2025-06-09 07:59:26 -0600

    add: html to vue-demo1

---
+++ vue-demo1/src/html/introCSS.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>MAIN</h1>
+    <p>this is the new text.</p>
+    <img src="../images/square.jpg" alt="Square lee" width="250px"/>
+    <p>This is a square Lee.</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+h1 {
+  color: rgb(188, 240, 158);
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  font-family: 'Courier New', Courier, monospace;
+}
+p {
+  color: rgb(0, 0, 0);
+  background-color: rgb(202, 254, 195);
+  padding: 10px;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 24px;
+  text-decoration: underline;
+  text-decoration-color: rgb(0, 0, 0);
+  text-decoration-thickness: 2px;
+  text-decoration-style: wavy;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  font-family: 'Courier New', Courier, monospace;
+  line-height: 1.5;
+  letter-spacing: 1px;
+  word-spacing: 2px;
+}
+div {
+  background-color: black;
+  padding: 10px;
+  border-radius: 5px;
+}
+h1,
+p {
+  text-align: center;
+}
+img {
+  display: block;
+  margin: 0 auto;
+  border-radius: 10px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ vue-demo1/src/html/introTables.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <table border="1">
+      <caption>
+        Table of Users
+      </caption>
+      <tr>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+      </tr>
+      <tr>
+        <td>Alfi</td>
+        <td>Sun</td>
+        <td>panchopistolas@mail.com</td>
+      </tr>
+      <tr>
+        <td>Mery</td>
+        <td>Sunflower</td>
+        <td>cheetito@mail.com</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+table,
+th,
+td {
+  border: 1px solid black;
+  border-spacing: 0;
+  border-radius: 5px;
+  text-align: center;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 16px;
+  background-color: rgb(202, 254, 195);
+  color: rgb(0, 0, 0);
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 50px;
+  border-collapse: collapse;
+  padding: 5px;
+}
+caption{
+    color: #000000;
+    font-family: 'Trocchi', serif;  
+    font-size: 25px;
+    line-height: 70px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 20px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    background-color: #e8e8e8;
+    padding: 10px;
+    border-radius: 10px;
+}
+</style>
